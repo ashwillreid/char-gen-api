0e87c0a383b48504c64c0c4310610f144d3985de
ashwillReid <reid@rockpapercoin.com>
2024-04-15 15:08:50 -0700

    go to charClass

--- src/controllers/characterControllers.ts
+++ src/controllers/characterControllers.ts
@@ -1,18 +1,36 @@
 import { Request, Response } from "express";
 import { prisma } from "../server";
+import { PrismaClient } from "@prisma/client";
+
+interface charCreateArgs {
+  name: string;
+  age: number;
+  race: string;
+  class: string;
+}
 
 const createCharacter = async (req: Request, res: Response) => {
   try {
-    const { name, age, race, class }  = req.body;
-    const newCharacter = await prisma.character.create({
-      data: {
-        name,
-        age,
-				race,
-				class
-      },
-    });
-    res.status(200).json(newCharacter);
+    if (
+      !!req?.body?.name ||
+      !!req?.body?.age ||
+      !!req?.body?.race ||
+      !!req?.body?.class
+    ) {
+      // const { name, age, race, class } = req.body;
+      const { name, age, race, class: charClass } = req.body;
+      const newCharacter = await prisma.character.create({
+        data: {
+          name,
+          age,
+          race,
+          class: charClass,
+        },
+      });
+      res.status(200).json(newCharacter);
+    } else {
+      throw new Error("YA DONE FLUBBED SON!");
+    }
   } catch (e) {
     res.status(500).json({ error: e });
   }
