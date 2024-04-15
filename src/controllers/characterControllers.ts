aff7bac5318242a64dfd69ee0504413ef885551e
ashwillReid <reid@rockpapercoin.com>
2024-04-15 16:40:43 -0700

    add getCharacterById

--- src/controllers/characterControllers.ts
+++ src/controllers/characterControllers.ts
@@ -32,6 +32,27 @@ const createCharacter = async (req: Request, res: Response) => {
   }
 };
 
+const getCharacterById = async (req: Request, res: Response) => {
+  console.log("up in there", req.body);
+
+  try {
+    if (req.params.id) {
+      const { id } = req.params;
+      const newCharacter = await prisma.character.findUnique({
+        where: { id },
+      });
+
+      console.log("CHARACTER????", newCharacter);
+      res.status(200).json(newCharacter);
+    } else {
+      throw new Error("YA DONE FLUBBED SON!");
+    }
+  } catch (e) {
+    console.log("error?", e);
+    res.status(500).json({ error: e });
+  }
+};
 export default {
   createCharacter,
+  getCharacterById,
 };
