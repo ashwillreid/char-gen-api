1cedb0cb642a56a25bb25746aa255bf285148068
ashwillReid <reid@rockpapercoin.com>
2024-04-16 16:44:59 -0700

    cleanup

--- src/controllers/characterControllers.ts
+++ src/controllers/characterControllers.ts
@@ -2,32 +2,28 @@ import { Request, Response } from "express";
 import { prisma } from "../server";
 
 const createCharacter = async (req: Request, res: Response) => {
-  console.log("up in there", req.body);
-
   try {
     if (
-      req.body.args.name &&
-      req.body.args.age &&
-      req.body.args.race &&
-      req.body.args.charClass
+      req.body?.args?.name &&
+      req.body?.args?.age &&
+      req.body?.args?.race &&
+      req.body?.args?.charClass
     ) {
       const { name, age, race, charClass } = req.body.args;
       const newCharacter = await prisma.character.create({
         data: {
           name,
-          age,
+          age: parseInt(age),
           race,
           charClass,
         },
       });
 
-      console.log("CHARACTER????", newCharacter);
       res.status(200).json(newCharacter);
     } else {
       throw new Error("YA DONE FLUBBED SON!");
     }
   } catch (e) {
-    console.log("error?", e);
     res.status(500).json({ error: e });
   }
 };
